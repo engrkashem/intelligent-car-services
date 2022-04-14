2934067d6298772f93b1a0950a8b68358ce7bb66
engrkashem <kashemaust@gmail.com>
2022-04-14 09:40:22 +0600

    fake json data is made, fetch them using custom hook named useServices, then mapped in Services components, then sent to Service component to show in the UI

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useServices from '../../../hooks/useServices';
+import Service from '../Service/Service';
 
 const Services = () => {
+
+    const [services] = useServices();
+
     return (
         <div>
-            <h2>Services: </h2>
+            <h2>Services: {services.length}</h2>
+            {
+                services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                ></Service>)
+            }
         </div>
     );
 };
